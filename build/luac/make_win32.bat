16ad774794ff214ef6fef37a50afb850c1853356
liuwenjie <liuwenjiexx@sina.com>
2021-11-28 19:05:47 +0800

    build luac.exe

--- build/luac/make_win32.bat
+++ build/luac/make_win32.bat
@@ -1,6 +1,18 @@
-mkdir build32 & pushd build32
-cmake -DLUAC_COMPATIBLE_FORMAT=ON -G "Visual Studio 14 2015" ..
+call ../windows.bat
+set OUTPUT=build\lua-5.3.5\x32
+set TARGET_DIR=x32
+
+if exist %OUTPUT% ( rmdir /Q/S %OUTPUT% )
+mkdir %OUTPUT% & pushd %OUTPUT%
+
+cmake -DLUAC_COMPATIBLE_FORMAT=ON -G %CMAKE_G% -A Win32 ../../../
 IF %ERRORLEVEL% NEQ 0 cmake -DLUAC_COMPATIBLE_FORMAT=ON -G "Visual Studio 15 2017" ..
 popd
-cmake --build build32 --config Release
+
+cmake --build %OUTPUT% --config Release
+
+mkdir %TARGET_DIR%
+move .\%OUTPUT%\Release\lua.exe %TARGET_DIR%\lua.exe
+move .\%OUTPUT%\Release\luac.exe %TARGET_DIR%\luac.exe
+
 pause
--- build/luac/make_win64.bat
+++ build/luac/make_win64.bat
@@ -1,6 +1,18 @@
-mkdir build64 & pushd build64
-cmake -DLUAC_COMPATIBLE_FORMAT=ON -G "Visual Studio 14 2015 Win64" ..
+call ../windows.bat
+set OUTPUT=build\lua-5.3.5\x64
+set TARGET_DIR=x64
+
+if exist %OUTPUT% ( rmdir /Q/S %OUTPUT% )
+mkdir %OUTPUT% & pushd %OUTPUT%
+
+cmake -DLUAC_COMPATIBLE_FORMAT=ON -G %CMAKE_G%  -A x64 ../../../
 IF %ERRORLEVEL% NEQ 0 cmake -DLUAC_COMPATIBLE_FORMAT=ON -G "Visual Studio 15 2017 Win64" ..
 popd
-cmake --build build64 --config Release
+
+cmake --build %OUTPUT% --config Release
+
+mkdir %TARGET_DIR%
+move .\%OUTPUT%\Release\lua.exe %TARGET_DIR%\lua.exe
+move .\%OUTPUT%\Release\luac.exe %TARGET_DIR%\luac.exe
+
 pause
